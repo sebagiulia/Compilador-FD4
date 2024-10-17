b33c6094e7776fe42c5fcd266a7fa02098f0c8f3
Mateo Bacci <mateobacci06@gmail.com>
2024-10-17 20:52:12 +0000

    Primera version de Elab. Faltan cosas

--- src/Elab.hs
+++ src/Elab.hs
@@ -14,6 +14,7 @@ module Elab ( elab, elabDecl) where
 
 import Lang
 import Subst
+import MonadFD4
 
 -- | 'elab' transforma variables ligadas en índices de de Bruijn
 -- en un término dado. 
@@ -28,25 +29,71 @@ elab' env (SV p v) =
     then return $ V p (Free v)
     else return $ V p (Global v)
 elab' _ (SConst p c) = return $ Const p c
-elab' env (SLam p [(v, ty)] t) = Lam p v ty (close v (elab' (v:env) t))
-elab' env (SLam p ((v, ty):args) t) = Lam p v ty (close v (elab' (v:env) (SLam p args t)))
+elab' env (SLam p [(v, ty)] t) = do
+    te <- elab' (v:env) t
+    let tc = close v te
+    tye <- elabTy ty
+    return $ Lam p v tye tc 
+elab' env (SLam p ((v, ty):args) t) = do
+    te <- elab' (v:env) (SLam p args t)
+    let tc = close v te
+    tye <- elabTy ty
+    return $ Lam p v tye tc
 elab' env (SLam p [] t) = undefined -- no entra nunca
-elab' env (SFix p (f,fty) args@[(x,xty)] t) = Fix p f fty x xty (close2 f x (elab' (x:f:env) t))
-elab' env (SFix p (f,fty) args@((x,xty):xs) t) = Fix p f fty x xty (close2 f x (elab' (x:f:env) (SLam p xs t)))
+elab' env (SFix p (f,fty) args@[(x,xty)] t) = do 
+    te <- elab' (x:f:env) t
+    ftye <- elabTy fty
+    xtye <- elabTy xty
+    let tc = close2 f x te
+    return $ Fix p f ftye x xtye tc
+elab' env (SFix p (f,fty) args@((x,xty):xs) t) = do
+    te <- elab' (x:f:env) (SLam p xs t)
+    let tc = close2 f x te
+    ftye <- elabTy fty
+    xtye <- elabTy xty
+    return $ Fix p f ftye x xtye tc 
 elab' env (SFix p f [] t) = undefined -- no entra nunca
-elab' env (SIfZ p c t e)         = IfZ p (elab' env c) (elab' env t) (elab' env e)
+elab' env (SIfZ p c t e) = do
+    cond <- elab' env c
+    t1   <- elab' env t
+    t2   <- elab' env e
+    return $ IfZ p cond t1 t2
 -- Operadores binarios
-elab' env (SBinaryOp i o t u) = BinaryOp i o (elab' env t) (elab' env u)
+elab' env (SBinaryOp i o t u) = do
+    op1 <- elab' env t
+    op2 <- elab' env u
+    return $ BinaryOp i o op1 op2
 -- Operador Print
-elab' env (SPrint i str) = Lam i "x" NatTy (close "x" (Print i str (elab' ("x":env) (SV i "x"))))  -- Se encapsula en una expresión lambda
---elab' env (SPrint i str) = App i (Lam i "x" NatTy (close "x" (Print i str (elab' ("x":env) (SV i "x"))))) (Const i (CNat 0))  -- Se encapsula en una expresión lambda
+elab' env (SPrint i str) = do
+    arg <- elab' ("x":env) (SV i "x")
+    let tc = close "x" (Print i str arg)
+    return $ Lam i "x" NatTy tc  -- Se encapsula en una expresión lambda
 -- Aplicaciones generales
-elab' env (SApp p (SPrint i str) a) = Print i str (elab' env a)
-elab' env (SApp p h a) = App p (elab' env h) (elab' env a)
-elab' env (SLet p (v,vty) def body) =  
-  Let p v vty (elab' env def) (close v (elab' (v:env) body))
-elab' env (SLetFun p False (f, fty) args t1 t2) = Let p f (foldr typeConverter fty args) (elab' env (SLam p args t1)) (close f (elab' (f:env) t2)) 
-elab' env (SLetFun p True (f, fty) args t1 t2) =  Let p f (foldr typeConverter fty args) (elab' env (SFix p (f,foldr typeConverter fty args) args t1)) (close f (elab' (f:env) t2))
+elab' env (SApp p (SPrint i str) a) = do
+    te <- elab' env a
+    return $ Print i str te
+elab' env (SApp p h a) = do 
+    fun <- elab' env h
+    arg <- elab' env a
+    return $ App p fun arg
+elab' env (SLet p (v,vty) def body) = do
+    te <- elab' (v:env) body
+    let tc = close v te
+    tt <- elab' env def
+    vtye <- elabTy vty
+    return $ Let p v vtye tt tc
+elab' env (SLetFun p False (f, fty) args t1 t2) = do
+    te1 <- elab' env (SLam p args t1)
+    te2 <- elab' (f:env) t2
+    let tc = close f te2
+    ftye <- elabTy fty
+    return $ Let p f (foldr typeConverter ftye args) te1 tc 
+elab' env (SLetFun p True (f, fty) args t1 t2) = do 
+    te2 <- elab' (f:env) t2
+    let tc = close f te2
+    te1 <- elab' env (SFix p (f,foldr typeConverter fty args) args t1)
+    ftye <- elabTy fty
+    return $ Let p f (foldr typeConverter ftye args) te1 tc
 
 elabDecl :: Decl STerm -> Decl Term
 elabDecl d@(Decl _ _ _ _ [] _) = fmap elab d
@@ -56,17 +103,16 @@ elabDecl d@(Decl p True f fty args t) = fmap elab (Decl p True f (foldr typeConv
 elabDeclTy :: MonadFD4 m => DeclTy STy -> m DeclTy Ty
 elabDeclTy d = undefined
 
-
 elabTy :: MonadFD4 m => STy -> m Ty
 elabTy SNatTy = return NatTy
 elabTy (SFunTy a b) = do 
       sa <- elabTy a
       sb <- elabTy b
       return $ FunTy sa sb 
-elabTy (SVarTy n) = 
+elabTy (SVarTy n) = undefined
 
-typeConverter :: (Name, Ty) -> Ty -> Ty
-typeConverter (_, t) tys = FunTy t tys
+-- typeConverter :: (Name, Ty) -> Ty -> Ty
+-- typeConverter (_, t) tys = FunTy t tys
 
 
 {-
