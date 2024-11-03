0e203b0e67fb9333510ef3cd8d8344a2fc9b6ebf
= <sebastiangiulianelli02@gmail.com>
2024-11-02 21:36:12 -0300

    macchina casi lista (falta que pase algunos tests)

--- src/Bytecompile.hs
+++ src/Bytecompile.hs
@@ -29,8 +29,8 @@ import Data.Char
 
 type Opcode = Int
 type Bytecode = [Int]
-data Val = I Int | Fun Env Bytecode | RA Env Bytecode
-type Stack = [Val]
+data Val = I Int | Fun Env Bytecode | RA Env Bytecode deriving Show
+type Stack = [Val] 
 type Env = [Val]
 
 {- Esta nueva representación de Bytecode es simplemente una lista de
@@ -103,51 +103,41 @@ showOps (PRINT:xs)       = let (msg,_:rest) = span (/=NULL) xs
                            in ("PRINT " ++ show (bc2string msg)) : showOps rest
 showOps (PRINTN:xs)      = "PRINTN" : showOps xs
 showOps (ADD:xs)         = "ADD" : showOps xs
-showOps (x:xs)           = show x : showOps xs
+showOps (IFZ:xs)           = "IFZ" : showOps xs
+showOps (x:xs)           = (show x): showOps xs
 
 showBC :: Bytecode -> String
 showBC = intercalate "; " . showOps
 
 bcc :: MonadFD4 m => TTerm -> m Bytecode
-bcc t = case t of
-  Const i (CNat n) -> return [CONST, n]
-  V i v -> case v of
-    Free n -> undefined
-    Global n -> undefined
-    Bound n -> return [ACCESS, n]
-  Lam i n ty (Sc1 t') -> do b <- bcc t'
-                            return $ [FUNCTION, length b] ++ b ++ [RETURN]
-  App i t1 t2 -> do b1 <- bcc t1
-                    b2 <- bcc t2
-                    return $ b1 ++ b2 ++ [CALL]
-  BinaryOp i op t1 t2 -> do b1 <- bcc t1
-                            b2 <- bcc t2
-                            return $ b1 ++ b2 ++ [binop2bc op]
-  Let i n ty t1 (Sc1 t2) -> do b1 <- bcc t1
-                               b2 <- bcc t2
-                               return $ b1 ++ [SHIFT] ++ b2 ++ [DROP]
-  Fix i f ty1 x ty2 (Sc2 t') -> do b <- bcc t'
-                                   return $ [FUNCTION, length b] ++ b ++ [RETURN, FIX]   
-  IfZ i c t1 t2 -> do c' <- bcc c
-                      t1' <- bcc t1
-                      t2' <- bcc t2
-                      let th = [FUNCTION, length t1'] ++ t1' ++ [RETURN]
-                      let el = [FUNCTION, length t2'] ++ t2' ++ [RETURN]
-                      return $ el ++ th ++ c' ++ [IFZ] 
-  Print i str arg -> do arg' <- bcc arg
-                        case arg' of 
-                          [CONST, n] -> return $ [PRINTN]
-                          _          -> return $ [PRINT] ++ (string2bc str) ++ [NULL]
-
-{-
-Regla de compilación:
-  C(ifz c t1 t2) = FUNCTION(C(t2));FUNCTION(C(t1));C(c);IFZ
-
-Reglas de ejecución:
-  <IFZ;c | e |   0  :(et, ct):(ee, ce):s>  -->  <ct | et | (e,c)RA:s>
-  <IFZ;c | e | (n+1):(et, ct):(ee, ce):s>  -->  <ce | ee | (e,c)RA:s>
-
--}
+bcc term = bcc' term >>= return . (++ [STOP])
+ where bcc' t = case t of
+        Const i (CNat n) -> return [CONST, n]
+        V i v -> case v of
+          Free n -> undefined
+          Global n -> undefined
+          Bound n -> return [ACCESS, n]
+        Lam i n ty (Sc1 t') -> do b <- bcc' t'
+                                  return $ [FUNCTION, length b + 1] ++ b ++ [RETURN]
+        App i t1 t2 -> do b1 <- bcc' t1
+                          b2 <- bcc' t2
+                          return $ b1 ++ b2 ++ [CALL]
+        BinaryOp i op t1 t2 -> do b1 <- bcc' t1
+                                  b2 <- bcc' t2
+                                  return $ b1 ++ b2 ++ [binop2bc op]
+        Let i n ty t1 (Sc1 t2) -> do b1 <- bcc' t1
+                                     b2 <- bcc' t2
+                                     return $ b1 ++ [SHIFT] ++ b2 ++ [DROP]
+        Fix i f ty1 x ty2 (Sc2 t') -> do b <- bcc' t'
+                                         return $ [FUNCTION, length b + 1] ++ b ++ [RETURN, FIX]   
+        IfZ i c t1 t2 -> do c' <- bcc' c
+                            t1' <- bcc' t1
+                            t2' <- bcc' t2
+                            let th = [FUNCTION, length t1' + 1] ++ t1' ++ [RETURN]
+                            let el = [FUNCTION, length t2' + 1 ] ++ t2' ++ [RETURN]
+                            return $ el ++ th ++ c' ++ [IFZ] 
+        Print i str arg -> do arg' <- bcc' arg
+                              return $ [PRINT] ++ (string2bc str) ++ [NULL] ++ arg' ++ [PRINTN]
 
 binop2bc :: BinaryOp -> Opcode
 binop2bc Add = ADD
@@ -194,13 +184,14 @@ macchina (FIX:cs) e ((Fun e' cf):s) = do let efix = (Fun efix cf): e'
                                          macchina cs e ((Fun efix cf):s) 
 macchina (SHIFT:cs) e (v:s) = macchina cs (v:e) s
 macchina (DROP:cs) e s = macchina cs (tail e) s
-macchina (PRINT:cs) e ((I n):s) = do printFD4 (show n)
-                                     macchina cs e s
-macchina (PRINTN:cs) e s = do let str = takeWhile (/=NULL) cs
-                              printFD4 $ bc2string str
-                              macchina (drop (length str) cs) e s
+macchina (PRINTN:cs) e ((I n):s) = do printFD4 (show n)
+                                      macchina cs e ((I n):s)
+macchina (PRINT:cs) e s = do let str = takeWhile (/=NULL) cs
+                             printFD4 $ bc2string str
+                             macchina (drop (length str) cs) e s
 macchina (NULL:cs) e s = macchina cs e s
-macchina (STOP:cs) e s = failFD4 "STOP"
-macchina (IFZ:cs) e ( I 0 : Fun e1 c1 : Fun e2 c2 : s) = macchina c1 e1 (RA e cs:s)
-macchina (IFZ:cs) e ( I n : Fun e1 c1 : Fun e2 c2 : s) = macchina c2 e2 (RA e cs:s)
-macchina _ e s = failFD4 "Instrucción inválida en la Macchina"
+macchina (STOP:cs) e s = printFD4 (show (head s))
+macchina (IFZ:cs) e ( I 0 : Fun e1 c1 : _ : s) = macchina c1 e1 (RA e cs:s)
+macchina (IFZ:cs) e ( I _ : _ : Fun e2 c2 : s) = macchina c2 e2 (RA e cs:s)
+macchina (c:_) e s = failFD4 $ "Instrucción inválida en la Macchina: " ++ (show c)
+macchina [] e s = failFD4 "No hay más instrucciones en la Macchina"
